0af99b3c8fc5f2d528200e2105191b0c031f60b0
Alwaleed <wlle-202@hotmail.com>
2026-01-22 01:01:49 +0300

    Add AppBar and Column

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,83 @@ class HomeScreen extends StatelessWidget {
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.purple,
+          backgroundColor: const Color.fromARGB(255, 131, 95, 247),
+          title: Text("Flutter App"),
+          centerTitle: true,
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(4),
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.flutter_dash),
+                      Text("Flutter"),
+                    ],
+                  ),
+
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.orange,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.purple,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.cyan,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.pink,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lime,
+                ),
+              ],
+            )
+        ])
       );
 
     }
