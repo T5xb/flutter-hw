aa2df6a042bc1a372b032f7120049e5f5dc99ce4
Alwaleed <wlle-202@hotmail.com>
2026-01-22 00:16:52 +0300

    Start flutter Code

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_hw_4
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hw_4/screens/home_screen.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
