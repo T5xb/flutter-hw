5b55da7624a65d89714e4aa528ac079f13a2df35
Alwaleed <wlle-202@hotmail.com>
2026-01-22 13:09:34 +0300

    Updated AppBar and add 1 row

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,148 +4,124 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 131, 95, 247),
-          title: Text("Flutter App"),
-          titleTextStyle: TextStyle(
-            color: const Color.fromARGB(255, 0, 0, 0),
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 131, 95, 247),
+        title: Text("Flutter App"),
+        titleTextStyle: TextStyle(
+          color: Color.fromARGB(255, 0, 0, 0),
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+        centerTitle: true,
+
+        leading: Icon(Icons.menu),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.account_circle_rounded),
+                    Text("Profile"),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.all_inbox), Text("Inbox")],
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.message), Text("Messages")],
+                ),
+              ),
+            ],
           ),
-          centerTitle: true,
-          
-          leading: Icon(Icons.menu),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.account_circle_rounded),
-                      Text("Profile"),
-                    ],
-                  ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.navigation), Text("Navigation")],
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.all_inbox),
-                      Text("Inbox"),
-                    ],
-                  )
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.notifications), Text("Notifications")],
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.message),
-                      Text("Messages"),
-                    ],
-                  )
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.paypal), Text("PayPal")],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.navigation),
-                      Text("Navigation"),
-                    ],
-                  )
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.contacts), Text("Contacts")],
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.notifications),
-                      Text("Notifications"),
-                    ],
-                  )
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.settings), Text("Settings")],
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.paypal),
-                      Text("PayPal"),
-                    ],
-                  )
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(53, 92, 75, 74),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.alarm), Text("Alarm")],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.contacts),
-                      Text("Contacts"),
-                    ],
-                  )
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.settings),
-                      Text("Settings"),
-                    ],
-                  )
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: const Color.fromARGB(53, 92, 75, 74),child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.alarm),
-                      Text("Alarm"),
-                    ],
-                  )
-                ),
-              ],
-            ),
-         ],
-        ),
-
-      );
-
-    }
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
